10d148e81d8b25ce314ccd5cff9ad011a7124349
Thales Pupo Araújo <thalespupo@gmail.com>
2018-03-05 05:48:50 -0300

    Adding fields for playing episode

--- app/src/main/java/com/tapura/podmorecasts/model/Episode.java
+++ app/src/main/java/com/tapura/podmorecasts/model/Episode.java
@@ -6,6 +6,10 @@ public class Episode {
     private String link;
     private String episodeLink;
     private String description;
+    private String pathInDisk;
+    private long episodeLength;
+    private long trackPosition;
+    private boolean alreadyListened;
 
     public String getTitle() {
         return title;
@@ -37,5 +41,37 @@ public class Episode {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getPathInDisk() {
+        return pathInDisk;
+    }
+
+    public void setPathInDisk(String pathInDisk) {
+        this.pathInDisk = pathInDisk;
+    }
+
+    public long getEpisodeLength() {
+        return episodeLength;
+    }
+
+    public void setEpisodeLength(long episodeLength) {
+        this.episodeLength = episodeLength;
+    }
+
+    public long getTrackPosition() {
+        return trackPosition;
+    }
+
+    public void setTrackPosition(long trackPosition) {
+        this.trackPosition = trackPosition;
+    }
+
+    public boolean isAlreadyListened() {
+        return alreadyListened;
+    }
+
+    public void setAlreadyListened(boolean alreadyListened) {
+        this.alreadyListened = alreadyListened;
     }
 }
