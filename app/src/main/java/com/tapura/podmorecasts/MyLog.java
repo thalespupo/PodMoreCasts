53ca76e1691339ec65101baa4cef339ae748f6fa
Thales Pupo Araújo <thalespupo@gmail.com>
2018-03-22 22:35:41 -0300

    Add method getlogtag in log class

--- app/src/main/java/com/tapura/podmorecasts/MyLog.java
+++ app/src/main/java/com/tapura/podmorecasts/MyLog.java
@@ -1,10 +1,6 @@
 package com.tapura.podmorecasts;
 
-import android.arch.lifecycle.Observer;
 import android.util.Log;
-import android.util.Pair;
-
-import com.tapura.podmorecasts.model.Podcast;
 
 
 public class MyLog {
@@ -12,26 +8,30 @@ public class MyLog {
     private static final String TAG = "PodcastApp::";
 
     public static void v(Class c, String msg) {
-        Log.v(TAG + c.getSimpleName(), msg);
+        Log.v(getLogTag(c), msg);
     }
 
     public static void d(Class c, String msg) {
-        Log.d(TAG + c.getSimpleName(), msg);
+        Log.d(getLogTag(c), msg);
     }
 
     public static void i(Class c, String msg) {
-        Log.i(TAG + c.getSimpleName(), msg);
+        Log.i(getLogTag(c), msg);
     }
 
     public static void w(Class c, String msg) {
-        Log.w(TAG + c.getSimpleName(), msg);
+        Log.w(getLogTag(c), msg);
     }
 
     public static void e(Class c, String msg) {
-        Log.e(TAG + c.getSimpleName(), msg);
+        Log.e(getLogTag(c), msg);
     }
 
     public static void wtf(Class c, String msg) {
-        Log.wtf(TAG + c.getSimpleName(), msg);
+        Log.wtf(getLogTag(c), msg);
+    }
+
+    public static String getLogTag(Class c) {
+        return TAG + c.getSimpleName();
     }
 }
