1761ecbca7b8cd587fe1d5e939a2ab75fe77a2c4
Thales Pupo Araújo <thalespupo@gmail.com>
2018-04-12 16:32:14 -0300

    Moving player to a service

--- app/src/main/java/com/tapura/podmorecasts/player/MediaPlayerActivity.java
+++ app/src/main/java/com/tapura/podmorecasts/player/MediaPlayerActivity.java
@@ -3,13 +3,17 @@ package com.tapura.podmorecasts.player;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.MediaStore;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.media.session.MediaButtonReceiver;
 import android.support.v4.media.session.MediaSessionCompat;
@@ -49,24 +53,23 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
 
-public class MediaPlayerActivity extends AppCompatActivity implements View.OnClickListener, Player.EventListener {
+public class MediaPlayerActivity extends AppCompatActivity {
 
     private static final String EXTRA_FEED_URL = "extra_feed_url";
     private static final String EXTRA_EPISODE_POS = "extra_episode_pos";
-    private static final String SIS_PLAYER_POSITION = "sys_player_position";
-    private static final String SIS_PLAYER_PLAYING = "sys_player_playing";
+    private static final String SIS_BOUND = "sis_bound";
     private FirebaseDb mDb;
     private Episode mEpisode;
     private String mFeed;
     private int mEpisodePos;
+    private File mMedia;
     private final ValueEventListener fbCallback = new ValueEventListener() {
         @Override
         public void onDataChange(DataSnapshot dataSnapshot) {
             if (dataSnapshot.exists()) {
                 mEpisode = dataSnapshot.getValue(Episode.class);
                 initializeUi();
-                initializeMediaSession();
-                initializePlayer();
+                initializeService();
             }
         }
 
@@ -75,15 +78,10 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
 
         }
     };
-    private SimpleExoPlayer mExoPlayer;
-    private SimpleExoPlayerView mPlayerView;
-    private static MediaSessionCompat mMediaSession;
-    private PlaybackStateCompat.Builder mStateBuilder;
-    private NotificationManager mNotificationManager;
-    private Bitmap mOutdoor;
-    private long mPlaybackPosition;
-    private boolean mIsPlaying;
 
+    private SimpleExoPlayerView mPlayerView;
+    private Bitmap mOutdoor;
+    private MediaPlayerService mService;
 
     public static Intent createIntent(Context context, String feedUrl, int pos) {
         Intent intent = new Intent(context, MediaPlayerActivity.class);
@@ -106,16 +104,9 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
         mPlayerView = findViewById(R.id.playerView);
 
         if (savedInstanceState != null) {
-            if (savedInstanceState.containsKey(SIS_PLAYER_POSITION)) {
-                mPlaybackPosition = savedInstanceState.getLong(SIS_PLAYER_POSITION);
-            }
-            if (savedInstanceState.containsKey(SIS_PLAYER_PLAYING)) {
-                mIsPlaying = savedInstanceState.getBoolean(SIS_PLAYER_PLAYING);
-            }
-        } else {
-            mPlaybackPosition = 0;
-            mIsPlaying = false;
+            bound = savedInstanceState.getBoolean(SIS_BOUND);
         }
+
     }
 
     @Override
@@ -124,31 +115,17 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
         if (mDb != null) {
             mDb.detachEpisodeListener(MyApplication.getApp(), mFeed, mEpisodePos, fbCallback);
         }
-        releasePlayer();
-        mMediaSession.setActive(false);
-    }
-
-    private void releasePlayer() {
-        mNotificationManager.cancelAll();
-        mExoPlayer.stop();
-        mExoPlayer.release();
-        mExoPlayer = null;
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        if (mExoPlayer != null) {
-            outState.putLong(SIS_PLAYER_POSITION, mExoPlayer.getCurrentPosition());
-            outState.putBoolean(SIS_PLAYER_PLAYING, mExoPlayer.getPlayWhenReady());
+        if (bound) {
+            unbindService(mConnection);
         }
-        super.onSaveInstanceState(outState);
     }
 
     private void initializeUi() {
+        mMedia = new File(Utils.getAbsolutePath(mEpisode.getPathInDisk()));
+        MyLog.d(getClass(), "initializeUi: media path=" + mMedia.getAbsolutePath());
 
-        MyLog.d(getClass(), "initializeUi: media path=" + Utils.getAbsolutePath(mEpisode.getPathInDisk()));
         MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-        mmr.setDataSource(Utils.getAbsolutePath(mEpisode.getPathInDisk()));
+        mmr.setDataSource(mMedia.getAbsolutePath());
         InputStream inputStream = null;
         if (mmr.getEmbeddedPicture() != null) {
             inputStream = new ByteArrayInputStream(mmr.getEmbeddedPicture());
@@ -159,211 +136,38 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
         mPlayerView.setDefaultArtwork(mOutdoor);
     }
 
-    private void initializeMediaSession() {
-
-        // Create a MediaSessionCompat.
-        mMediaSession = new MediaSessionCompat(this, MediaPlayerActivity.class.getSimpleName());
-
-        // Enable callbacks from MediaButtons and TransportControls.
-        mMediaSession.setFlags(
-                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
-                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
-
-        // Do not let MediaButtons restart the player when the app is not visible.
-        mMediaSession.setMediaButtonReceiver(null);
-
-        // Set an initial PlaybackState with ACTION_PLAY, so media buttons can start the player.
-        mStateBuilder = new PlaybackStateCompat.Builder()
-                .setActions(
-                        PlaybackStateCompat.ACTION_PLAY |
-                                PlaybackStateCompat.ACTION_PAUSE |
-                                PlaybackStateCompat.ACTION_REWIND |
-                                PlaybackStateCompat.ACTION_FAST_FORWARD |
-                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
-
-        mMediaSession.setPlaybackState(mStateBuilder.build());
-
-
-        // MySessionCallback has methods that handle callbacks from a media controller.
-        mMediaSession.setCallback(new MySessionCallback());
-
-        // Start the Media Session since the activity is active.
-        mMediaSession.setActive(true);
+    private void initializeService() {
+        MyLog.d(getClass(), "initializeService");
+        Intent intent = new Intent(this, MediaPlayerService.class);
+        startService(intent);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
     }
 
-    private void initializePlayer() {
-        Uri mediaUri = Uri.parse(new File(Utils.getAbsolutePath(mEpisode.getPathInDisk())).toString());
-        if (mExoPlayer == null) {
-            // Create an instance of the ExoPlayer.
-            TrackSelector trackSelector = new DefaultTrackSelector();
-            LoadControl loadControl = new DefaultLoadControl();
-            mExoPlayer = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this), trackSelector, loadControl);
-            mPlayerView.setPlayer(mExoPlayer);
-
-            // Set the ExoPlayer.EventListener to this activity.
-            mExoPlayer.addListener(this);
-
-            // Prepare the MediaSource.
-            String userAgent = Util.getUserAgent(this, "PodMoreCasts");
-            MediaSource mediaSource = new ExtractorMediaSource.Factory
-                    (new DefaultDataSourceFactory(this, userAgent)).createMediaSource(mediaUri);
-            mExoPlayer.seekTo(mPlaybackPosition);
-            mExoPlayer.prepare(mediaSource);
-            mExoPlayer.setPlayWhenReady(mIsPlaying);
-        }
-    }
-
-    private void showNotification(PlaybackStateCompat state) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "");
-
-        int icon;
-        String play_pause;
-        if (state.getState() == PlaybackStateCompat.STATE_PLAYING) {
-            icon = R.drawable.exo_controls_pause;
-            play_pause = getString(R.string.pause);
-        } else {
-            icon = R.drawable.exo_controls_play;
-            play_pause = getString(R.string.play);
-        }
-
-
-        NotificationCompat.Action playPauseAction = new NotificationCompat.Action(
-                icon, play_pause,
-                MediaButtonReceiver.buildMediaButtonPendingIntent(this,
-                        PlaybackStateCompat.ACTION_PLAY_PAUSE));
-
-        NotificationCompat.Action rewindButton = new android.support.v4.app.NotificationCompat
-                .Action(R.drawable.exo_controls_rewind, getString(R.string.rewind),
-                MediaButtonReceiver.buildMediaButtonPendingIntent
-                        (this, PlaybackStateCompat.ACTION_REWIND));
-
-        NotificationCompat.Action fastForwardAction = new android.support.v4.app.NotificationCompat
-                .Action(R.drawable.exo_controls_fastforward, getString(R.string.forward),
-                MediaButtonReceiver.buildMediaButtonPendingIntent
-                        (this, PlaybackStateCompat.ACTION_FAST_FORWARD));
-
-        PendingIntent contentPendingIntent = PendingIntent.getActivity
-                (this, 0, new Intent(this, MediaPlayerActivity.class), 0);
-
-        builder.setContentTitle(getString(R.string.app_name))
-                .setContentText(mEpisode.getTitle())
-                .setContentIntent(contentPendingIntent)
-                .setSmallIcon(R.drawable.ic_headset_black)
-                .setLargeIcon(mOutdoor)
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                .addAction(rewindButton)
-                .addAction(playPauseAction)
-                .addAction(fastForwardAction)
-                .setStyle(new android.support.v4.media.app.NotificationCompat.MediaStyle()
-                        .setMediaSession(mMediaSession.getSessionToken())
-                        .setShowActionsInCompactView(0, 1, 2));
-
-
-        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        mNotificationManager.notify(0, builder.build());
-    }
-
-    private class MySessionCallback extends MediaSessionCompat.Callback {
+    private boolean bound;
+    private ServiceConnection mConnection = new ServiceConnection() {
         @Override
-        public void onPlay() {
-            mExoPlayer.setPlayWhenReady(true);
-        }
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MyLog.d(getClass(), "onServiceConnected");
+            bound = true;
+            MediaPlayerService.LocalBinder binder = (MediaPlayerService.LocalBinder) service;
+            mService = binder.getService();
 
-        @Override
-        public void onPause() {
-            mExoPlayer.setPlayWhenReady(false);
-        }
-
-        @Override
-        public void onRewind() {
-            long rewindTime = getResources().getInteger(R.integer.rewind);
-            long currentPos = mExoPlayer.getCurrentPosition();
-            if (currentPos <= rewindTime) {
-                mExoPlayer.seekTo(0);
-            } else {
-                mExoPlayer.seekTo(currentPos - rewindTime);
+            if (!mService.wasInitialized()) {
+                mService.initializeService(mEpisode, mOutdoor);
             }
+
+            mPlayerView.setPlayer(mService.getPlayer());
         }
 
         @Override
-        public void onFastForward() {
-            long fastForward = getResources().getInteger(R.integer.fast_forward);
-            mExoPlayer.seekTo(mExoPlayer.getCurrentPosition() + fastForward);
+        public void onServiceDisconnected(ComponentName name) {
+            bound = false;
         }
-    }
-
-
-    @Override
-    public void onClick(View v) {
-
-    }
+    };
 
     @Override
-    public void onTimelineChanged(Timeline timeline, Object manifest) {
-
-    }
-
-    @Override
-    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-
-    }
-
-    @Override
-    public void onLoadingChanged(boolean isLoading) {
-
-    }
-
-    @Override
-    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-        if ((playbackState == Player.STATE_READY) && playWhenReady) {
-            mStateBuilder.setState(PlaybackStateCompat.STATE_PLAYING,
-                    mExoPlayer.getCurrentPosition(), 1f);
-        } else if ((playbackState == Player.STATE_READY)) {
-            mStateBuilder.setState(PlaybackStateCompat.STATE_PAUSED,
-                    mExoPlayer.getCurrentPosition(), 1f);
-        }
-        mMediaSession.setPlaybackState(mStateBuilder.build());
-        showNotification(mStateBuilder.build());
-    }
-
-    @Override
-    public void onRepeatModeChanged(int repeatMode) {
-
-    }
-
-    @Override
-    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
-
-    }
-
-    @Override
-    public void onPlayerError(ExoPlaybackException error) {
-
-    }
-
-    @Override
-    public void onPositionDiscontinuity(int reason) {
-
-    }
-
-    @Override
-    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-
-    }
-
-    @Override
-    public void onSeekProcessed() {
-
-    }
-
-    public static class MediaReceiver extends BroadcastReceiver {
-
-        public MediaReceiver() {
-        }
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            MediaButtonReceiver.handleIntent(mMediaSession, intent);
-        }
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(SIS_BOUND, bound);
+        super.onSaveInstanceState(outState);
     }
 }
---
+++ app/src/main/java/com/tapura/podmorecasts/player/MediaPlayerService.java
@@ -0,0 +1,332 @@
+package com.tapura.podmorecasts.player;
+
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.media.session.MediaButtonReceiver;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+import com.tapura.podmorecasts.R;
+import com.tapura.podmorecasts.Utils;
+import com.tapura.podmorecasts.model.Episode;
+
+import java.io.File;
+
+
+public class MediaPlayerService extends Service implements Player.EventListener {
+
+    private static final String ACTION_NOTIFICATION_DELETED = "action_notification_deleted";
+
+    private static MediaSessionCompat mMediaSession;
+    private PlaybackStateCompat.Builder mStateBuilder;
+    private SimpleExoPlayer mExoPlayer;
+    private NotificationManager mNotificationManager;
+
+    private final IBinder mBinder = new LocalBinder();
+    private Episode mEpisode;
+    private Bitmap mOutdoor;
+
+    public SimpleExoPlayer getPlayer() {
+        return mExoPlayer;
+    }
+
+    public boolean wasInitialized() {
+        return mEpisode != null;
+    }
+
+    public class LocalBinder extends Binder {
+        MediaPlayerService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return MediaPlayerService.this;
+        }
+    }
+
+    @Override
+    public ComponentName startService(Intent service) {
+        if (service != null) {
+            if (service.getAction() != null && service.getAction().equals(ACTION_NOTIFICATION_DELETED)) {
+                stopSelf();
+            }
+        }
+        return super.startService(service);
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        releasePlayer();
+        if (mNotificationManager != null) {
+            mNotificationManager.cancelAll();
+        }
+
+        if (mMediaSession != null) {
+            mMediaSession.setActive(false);
+        }
+
+        super.onDestroy();
+    }
+
+    private void releasePlayer() {
+        if (mExoPlayer != null) {
+            mExoPlayer.stop();
+            mExoPlayer.release();
+            mExoPlayer = null;
+        }
+    }
+
+    public void initializeService(@NonNull Episode episode, Bitmap outdoor) {
+        mEpisode = episode;
+        mOutdoor = outdoor;
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_NOTIFICATION_DELETED);
+
+        initializeMediaSession();
+        initializePlayer();
+    }
+
+    private void initializeMediaSession() {
+
+        // Create a MediaSessionCompat.
+        mMediaSession = new MediaSessionCompat(this, MediaPlayerActivity.class.getSimpleName());
+
+        // Enable callbacks from MediaButtons and TransportControls.
+        mMediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        // Do not let MediaButtons restart the player when the app is not visible.
+        mMediaSession.setMediaButtonReceiver(null);
+
+        // Set an initial PlaybackState with ACTION_PLAY, so media buttons can start the player.
+        mStateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PAUSE |
+                                PlaybackStateCompat.ACTION_REWIND |
+                                PlaybackStateCompat.ACTION_FAST_FORWARD |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+
+
+        // MySessionCallback has methods that handle callbacks from a media controller.
+        mMediaSession.setCallback(new MySessionCallback());
+
+        // Start the Media Session since the activity is active.
+        mMediaSession.setActive(true);
+    }
+
+    private void initializePlayer() {
+        Uri mediaUri = Uri.parse(new File(Utils.getAbsolutePath(mEpisode.getPathInDisk())).toString());
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this), trackSelector, loadControl);
+
+            // Set the ExoPlayer.EventListener to this activity.
+            mExoPlayer.addListener(this);
+
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(this, "PodMoreCasts");
+            MediaSource mediaSource = new ExtractorMediaSource.Factory
+                    (new DefaultDataSourceFactory(this, userAgent)).createMediaSource(mediaUri);
+            mExoPlayer.seekTo(0);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private void showNotification(PlaybackStateCompat state) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "");
+
+        int icon;
+        String play_pause;
+        if (state.getState() == PlaybackStateCompat.STATE_PLAYING) {
+            icon = R.drawable.exo_controls_pause;
+            play_pause = getString(R.string.pause);
+        } else {
+            icon = R.drawable.exo_controls_play;
+            play_pause = getString(R.string.play);
+        }
+
+        NotificationCompat.Action playPauseAction = new NotificationCompat.Action(
+                icon, play_pause,
+                MediaButtonReceiver.buildMediaButtonPendingIntent(this,
+                        PlaybackStateCompat.ACTION_PLAY_PAUSE));
+
+        NotificationCompat.Action rewindButton = new android.support.v4.app.NotificationCompat
+                .Action(R.drawable.exo_controls_rewind, getString(R.string.rewind),
+                MediaButtonReceiver.buildMediaButtonPendingIntent
+                        (this, PlaybackStateCompat.ACTION_REWIND));
+
+        NotificationCompat.Action fastForwardAction = new android.support.v4.app.NotificationCompat
+                .Action(R.drawable.exo_controls_fastforward, getString(R.string.forward),
+                MediaButtonReceiver.buildMediaButtonPendingIntent
+                        (this, PlaybackStateCompat.ACTION_FAST_FORWARD));
+
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (this, 0, new Intent(this, MediaPlayerActivity.class), 0);
+
+        Intent notificationDeleted = new Intent(this, MediaPlayerService.class);
+        notificationDeleted.setAction(ACTION_NOTIFICATION_DELETED);
+
+        PendingIntent pendingNotificationDeleted = PendingIntent.getService(this, 0, notificationDeleted, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        builder.setContentTitle(getString(R.string.app_name))
+                .setContentText(mEpisode.getTitle())
+                .setContentIntent(contentPendingIntent)
+                .setSmallIcon(R.drawable.ic_headset_black)
+                .setLargeIcon(mOutdoor)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .addAction(rewindButton)
+                .addAction(playPauseAction)
+                .addAction(fastForwardAction)
+                .setDeleteIntent(pendingNotificationDeleted)
+                .setStyle(new android.support.v4.media.app.NotificationCompat.MediaStyle()
+                        .setMediaSession(mMediaSession.getSessionToken())
+                        .setShowActionsInCompactView(0, 1, 2));
+
+
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, builder.build());
+    }
+
+
+    private class MySessionCallback extends MediaSessionCompat.Callback {
+        @Override
+        public void onPlay() {
+            mExoPlayer.setPlayWhenReady(true);
+        }
+
+        @Override
+        public void onPause() {
+            mExoPlayer.setPlayWhenReady(false);
+        }
+
+        @Override
+        public void onRewind() {
+            long rewindTime = getResources().getInteger(R.integer.rewind);
+            long currentPos = mExoPlayer.getCurrentPosition();
+            if (currentPos <= rewindTime) {
+                mExoPlayer.seekTo(0);
+            } else {
+                mExoPlayer.seekTo(currentPos - rewindTime);
+            }
+        }
+
+        @Override
+        public void onFastForward() {
+            long fastForward = getResources().getInteger(R.integer.fast_forward);
+            mExoPlayer.seekTo(mExoPlayer.getCurrentPosition() + fastForward);
+        }
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if ((playbackState == Player.STATE_READY) && playWhenReady) {
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PLAYING,
+                    mExoPlayer.getCurrentPosition(), 1f);
+        } else if ((playbackState == Player.STATE_READY)) {
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PAUSED,
+                    mExoPlayer.getCurrentPosition(), 1f);
+        }
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+        showNotification(mStateBuilder.build());
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
+
+    }
+
+    public static class MediaReceiver extends BroadcastReceiver {
+
+        public MediaReceiver() {
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            MediaButtonReceiver.handleIntent(mMediaSession, intent);
+        }
+    }
+}
